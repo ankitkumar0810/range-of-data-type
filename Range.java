50ccc6c2a8023e9af9071c1013ea7aaaf5c64083
Ankit Kumar <63963175+ankitkumar0810@users.noreply.github.com>
2021-02-25 14:51:59 +0530

    Add files via upload

---
+++ Range.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Range {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Range of byte is from " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("Range of short is from " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("Range of int is from " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("Range of long is from " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println("Range of float is from " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("Range of double is from " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        System.out.println("Range of char is from " + Character.MIN_VALUE + " to " + Character.MAX_VALUE);
+        //System.out.println("Range of boolean is from " + Boolean.MINVALUE + " to " + Byte.MAX_VALUE); Not associated with boolean
+
+    }
+    
+}
